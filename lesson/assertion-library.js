23225431b421635467ab67962a63a7ec849024de
naoya7076 <naoya7076@gmail.com>
2021-01-03 23:13:18 +0900

    update

--- lesson/assertion-library.js
+++ lesson/assertion-library.js
@@ -1,8 +1,9 @@
-const sum = (a ,b) => a + b
-const subtract = (a ,b) => a - b
+const {sum, subtract} = require('../math')
 
-let result = sum(3,7)
-let expected = 10
+let result, expected
+
+result = sum(3,7)
+expected = 10
 
 if(result !== expected){
   throw new Error(`${result} is not equal to ${expected}`)
