cc6fb071e7773b4cca03fee5b51e0f793875bfc2
naoya7076 <naoya7076@gmail.com>
2021-01-04 11:26:51 +0900

    rewrote more abstract

--- lesson/assertion-library.js
+++ lesson/assertion-library.js
@@ -2,19 +2,6 @@ const {sum, subtract} = require('../math')
 
 let result, expected
 
-result = sum(3,7)
-expected = 10
-
-if(result !== expected){
-  throw new Error(`${result} is not equal to ${expected}`)
-} 
-
-result = subtract(3,7)
-expected = -4
-if(result !== expected){
-  throw new Error(`${result} is not equal to ${expected}`)
-} 
-
 function expect(actual) {
   return {
     toBe(expected){
@@ -26,3 +13,11 @@ function expect(actual) {
     toBeGreaterThan(expected){},
   }
 }
+
+result = sum(3,7)
+expected = 10
+expect(result).toBe(expected)
+
+result = subtract(3,7)
+expected = -4
+expect(result).toBe(expected)
